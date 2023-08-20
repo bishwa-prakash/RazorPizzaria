a624c202f55c2adc4631b994b7f7bb871d23df8f
bishwa-prakash <sahoo.bishwaprakash2@gmail.com>
2023-08-20 08:31:52 +0530

    Add project files.

---
+++ RazorPizzaria/Pages/Pizza.cshtml.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzaria.Models;
+
+namespace RazorPizzaria.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public List<PizzasModel> fakePizzaDB=new List<PizzasModel>()
+        {
+            new PizzasModel(){
+                ImageTitle="Margerita",
+                PizzaName="Margerita",
+                BasePrice=2,
+                TamatoSauce=true,
+                Cheese=true,
+                FinalPrice=4},
+            new PizzasModel(){
+                ImageTitle="Bolognese",
+                PizzaName="Bolognese",
+                BasePrice=2,
+                TamatoSauce=true,
+                Cheese=true,
+                FinalPrice=4},
+            new PizzasModel(){
+                ImageTitle="Hawaiian",
+                PizzaName="Hawaiian",
+                BasePrice=2,
+                Cheese=true,
+                FinalPrice=4}, 
+            new PizzasModel(){
+                ImageTitle="Carbonara",
+                PizzaName="Carbonara",
+                BasePrice=2,
+                Beef=true,
+                Cheese=true,
+                FinalPrice=4},
+            new PizzasModel(){
+                ImageTitle="MeatFeast",
+                PizzaName="MeatFeast",
+                BasePrice=2,
+                Beef=true,
+                Cheese=true,
+                FinalPrice=4},
+            new PizzasModel(){
+                ImageTitle="Mushroom",
+                PizzaName="Mushroom",
+                BasePrice=2,
+                Beef=true,
+                Cheese=true,
+                FinalPrice=4},
+            new PizzasModel(){
+                ImageTitle="Pepperoni",
+                PizzaName="Pepperoni",
+                BasePrice=2,
+                Beef=true,
+                Cheese=true,
+                FinalPrice=4},
+            new PizzasModel(){
+                ImageTitle="Seafood",
+                PizzaName="Seafood",
+                BasePrice=2,
+                Beef=true,
+                Cheese=true,
+                FinalPrice=4},
+            new PizzasModel(){
+                ImageTitle="Vegetarian",
+                PizzaName="Vegetarian",
+                BasePrice=2,
+                Beef=true,
+                Cheese=true,
+                FinalPrice=4},
+
+        };
+        public void OnGet()
+        {
+        }
+    }
+}
